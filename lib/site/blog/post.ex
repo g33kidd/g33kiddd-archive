8fc14449c345fb6f8f153be519956a2792db5cd0
Joshua Kidd <kidd.josh.343@gmail.com>
2018-10-27 11:28:51 -0500

    Slugs

--- lib/site/blog/post.ex
+++ lib/site/blog/post.ex
@@ -8,10 +8,10 @@ defmodule Site.Blog.Post do
   # TODO: Add author/user
   # TODO: Add feature image
   schema "posts" do
-    field(:body, :string)
-    field(:slug, :string)
-    field(:title, :string)
-    field(:user_id, :id)
+    field :body, :string
+    field :slug, :string
+    field :title, :string
+    field :user_id, :id
 
     timestamps()
   end
@@ -21,11 +21,39 @@ defmodule Site.Blog.Post do
     post
     |> cast(attrs, [:title, :body, :slug])
     |> validate_required([:title, :body, :slug])
+    |> add_slug()
     |> unique_constraint(:slug)
+  end
+
+  def slugified_title(title) do
+    title
+    |> String.downcase()
+    |> String.replace(~r/[^A-Za-z0-9\s-]/, "")
+    |> String.replace(~r/(\s|-)+/, "-")
+  end
+
+  def generate_slug do
+    16
+    |> :crypto.strong_rand_bytes()
+    |> :base64.encode()
+    |> String.replace(~r/[^A-Za-z0-9]/, "")
+    |> String.slice(0, 10)
+    |> String.downcase()
   end
 
   def published(query) do
     from post in query,
       where: post.published == true
+  end
+
+  defp add_slug(changeset) do
+    case get_field(changeset, :slug) do
+      nil ->
+        generate_slug()
+        |> (&put_change(changeset, :slug, &1)).()
+
+      _ ->
+        changeset
+    end
   end
 end
